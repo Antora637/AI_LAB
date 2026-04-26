2a0202470c88711a7629b31c803365ffe85b4ee0
Antora637 <habibaontora01@gmail.com>
2026-04-26 15:00:58 +0600

    added DFS

--- DFS.cpp
+++ DFS.cpp
@@ -1,12 +1,50 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    cout << n;
-    for(int i=0;i<n;i++){
-        cout << i << endl;
+void dfs(int i, int v, vector<vector<int>> &adj_mat, vector<bool> &visited)
+{
+    visited[i] = true;
+    cout << i << " ";
+    for (int j : adj_mat[i])
+    {
+        if (!visited[j])
+            dfs(j, v, adj_mat, visited);
     }
-    return 0;
+}
+
+void bfs(int i, int v, vector<vector<int>> &adj_mat, vector<bool> &visited){
+    queue<int> Q;
+    Q.push(i);
+
+    while(!Q.empty()){
+        int u = Q.front();
+        cout << u << " ";
+        Q.pop();
+        visited[u] = true;
+
+        for(auto j : adj_mat[u]) {
+            if(!visited[j]){
+                visited[j] = true;
+                Q.push(j);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int v, e;
+    cin >> v >> e;
+    vector<vector<int>> adj_mat(v);
+    vector<bool> visited(v, false);
+
+    for (int i = 0; i < e; i++)
+    {
+        int u, w;
+        cin >> u >> w;
+        adj_mat[u].push_back(w);
+        adj_mat[w].push_back(u);
+    }
+    
+    bfs(0, v, adj_mat, visited);
 }
